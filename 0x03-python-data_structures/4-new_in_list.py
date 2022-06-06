afcab439a70d4c5b1a96968262ce716516ff0416
KoketsoMalatji <koketsosean209@gmail.com>
2022-06-06 16:12:08 -0700

    commit m

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 
-
 def new_in_list(my_list, idx, element):
-
-    if (idx < 0) or (idx > (len(my_list)-1)):
+    if 0 <= idx > (len(my_list)):
         return my_list
-
-    new_list = [x for x in my_list]
+    new_list = [j for j in my_list]
     new_list[idx] = element
-    return new_list
+    return new_element
