8da6736f6a19804451c541386b0f68a47f34dabc
KoketsoMalatji <koketsosean209@gmail.com>
2022-06-07 09:53:06 -0700

    reverse

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
-    new = ""
-    for n in range(len(my_string)):
-        if (my_string[n] != 'c' and my_string[n] != 'C'):
-            new += my_string[n]
-            return new
+    copy_str = [x for x in my_string if x != 'c' or x != 'C']
+    return ("".join(copy_str))
