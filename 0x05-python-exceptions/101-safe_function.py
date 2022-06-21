994c9214be2ba7bd11ae694af63a94bca8d0e2d2
KoketsoMalatji <koketsosean209@gmail.com>
2022-06-21 06:00:56 -0700

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], n=0):
+def safe_print_list(my_list=[], x=0):
     result = 0
-    for i in range(n):
+    for i in range(x):
         try:
             print(my_list[i], end='')
             result += 1
