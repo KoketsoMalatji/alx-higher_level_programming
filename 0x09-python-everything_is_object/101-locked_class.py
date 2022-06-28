cd6f333f5468a87950f13c0264a023d78759b9f5
KoketsoMalatji <koketsosean209@gmail.com>
2022-06-28 16:17:49 -0700

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-'''class that rejcts __dict__ and uses slots instead'''
+'''A class that rejcts __dict__ and uses slots instead'''
+
+
 class LockedClass:
-    '''this is the locked class that lets user create instance attribute'''
+    '''The locked class'''
+
     __slots__ = "first_name"
