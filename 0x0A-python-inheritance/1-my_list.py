25374d399aa18174d231c199c9263affdcf37b31
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-04 16:08:18 -0700

    Same class or inherit from

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module for 1-my_lis"""
-
-
-class MyList(list):
-    """my own class: MyList, inherits from list"""
-    def print_sorted(self):
-        """print sorted list method"""
-        print(sorted(self))
