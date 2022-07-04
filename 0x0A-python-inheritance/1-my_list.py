baab6fb136031a196deef303bdfc29ab864c74b0
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-04 15:25:46 -0700

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""module for 1-my_list.py"""
+'''module for 1-my_list'''
 
 
 class MyList(list):
-    """Class with method print_sorted"""
-    pass
+    """class utilizing list class"""
 
     def print_sorted(self):
-        """Methot that sorted a list"""
+        """print sorted list"""
+        my_list = list.copy(self)
+        list.sort(my_list)
+        print(my_list)
