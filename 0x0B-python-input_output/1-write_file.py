082b70912457a9be26d76ff74174192cda351f22
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-05 16:19:04 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""module for 1-number_of_lines """
+'''module for 1-number_of_lines'''
 
 
-def number_of_lines(filename=""):
-    """to read # of lines"""
-    with open(filename, "r", encoding="utf-8") as content:
-        return len(content.readlines())
+def write_file(filename="", text=""):
+    '''writes a string to  file overwrites if exist'''
+    with open(filename, mode='w', encoding='utf-8') as f:
+        fil = f.write(text)
+
+    return fil
