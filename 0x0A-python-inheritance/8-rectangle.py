af12343b8caa99ca69da176f90b84db5bd9845e2
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-06 16:05:18 -0700

    8-rec

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-'''task 8 module'''
-
-
+``"""module for Rectangle, class what inherits from BG - no test cases"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''rectangle class'''
+    """class rectangle is a subclass of BG"""
     def __init__(self, width, height):
-        '''Initialization of the object'''
-        super().integer_validator('width', width)
-        super().integer_validator('height', height)
+        """init method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
