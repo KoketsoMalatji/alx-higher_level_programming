251ea42ed9c818f0102dec38def3127329404fdf
KoketsoMalatji <koketsosean209@gmail.com>
2022-07-06 16:57:33 -0700

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-'''module for student'''
+"""module for student"""
 
 
 class Student:
-    '''class student'''
+    """a class we shall json"""
     def __init__(self, first_name, last_name, age):
-        '''Initialization of the student object'''
+        """init method"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        '''return a dictionary representation and all attributes must be retrieved
-        '''
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except Exception:
-            return self.__dict__
-        my_dict = dict()
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                my_dict[key] = value
-        return my_dict
+    def to_json(self):
+        """get dict"""
+        return self.__dict__.copy()
