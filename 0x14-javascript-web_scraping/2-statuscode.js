515fcdce8db3bdd6140d4a5f04a254f90309c701
KoketsoMalatji <koketsosean209@gmail.com>
2022-10-18 04:32:50 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+""" display the status code of a GET request"""
+const request = require('request');
+request(process.argv[2], (err, response) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
