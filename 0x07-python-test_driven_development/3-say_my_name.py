db0b23c0df504f22ec51996df7de1484f2b444a0
Koketso Malatji <koketsosean209@gmail.com>
2022-11-16 01:00:44 +0200

    test driven

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-'''This module for printing names'''
+"""
+a module
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    '''This function print the string "My name is <first name> <last name>"
-    args:
-        first_name (str): first name
-        last_name (str): last name
-    returns:
-        None
-    '''
-
+    """
+    Says your name.
+    first_name and last_name must be strings otherwise, will raise a TypeError
+    """
+    # first_name must be an integer
     if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    elif type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-
-    print(f'My name is {first_name} {last_name}')
+        raise TypeError("first_name must be a string")
+    # last_name must be an integer
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))
